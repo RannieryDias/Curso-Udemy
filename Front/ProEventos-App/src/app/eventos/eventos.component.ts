0154931de86e55bcf91fc67cc14384b5d50d9c38
Ranniery Dias <ranniery.dias@live.com>
2023-03-24 21:01:22 -0300

    Added an Angular page to use the Get from the Rest

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent {
-  public eventos: any ;
+  public eventos: any = [];
+  public filteredEventos: any = [];
+  isImageCollapsed: boolean = false;
+
+  widthImg: number = 50;
+  marginImg: number = 2;
+
+
+  private _filterList: string = '';
+
+  public get filterList(){
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value
+    this.filteredEventos = this.filterList ?
+                                    this.filterEvent(this.filterList) :
+                                    this.eventos;
+  }
+
+  private filterEvent(filterBy: string) : any{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase()
+                           .indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,9 +43,13 @@ export class EventosComponent {
     this.getEventos();
   }
 
+  changeIsImageCollapsedEvent(){
+    this.isImageCollapsed = !this.isImageCollapsed;
+  }
+
   public getEventos(): void {
     this.http.get('http://localhost:5216/api/eventos').subscribe({
-      next: (response) => this.eventos = response,
+      next: (response) => this.filteredEventos = this.eventos = response,
       error: (e) => console.log(e)
     });
 
